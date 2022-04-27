dc3406a5fc24e80779b55978de98bd781476ffcd
venketeswar664 <98304893+venketeswar664@users.noreply.github.com>
2022-04-28 01:51:16 +0530

    task completed

--- Python/task/venketeswar/code.py
+++ Python/task/venketeswar/code.py
@@ -1,40 +1,41 @@
 import random
 while True:
-    player1 = input("Player 1 enter your name ==> ")
-    player2 = input("Player 2 enter your name ==> ")
-    target = int(input("Enter the target number: "))
-    score1 = 0
-    score2 = 0
+    NAME1 = input("ENTER NAME'S OF PLAYER1:=> ")
+    NAME2 = input("ENTER NAME'S OF PLAYER2:=> ")
+    GOAL= int(input("DECIDE THE TARGET:=> "))
+    TOTAL_player1 = 0
+    TOTAL_player2 = 0
     while True:
-        if (score1 < target) and (score2 < target):
-            input(f"{player1} press Enter to roll the dice.")
+        if (TOTAL_player1 < GOAL) and (TOTAL_player2 < GOAL):
+            input(f"{NAME1} press Enter to roll the dice.")
             dice1 = int(random.randint(1, 6))
             print(f"Dice rolled {dice1}")
-            score1 = score1+dice1
-            input(f"{player2} press enter to roll the dice.")
+            TOTAL_player1 = TOTAL_player1+dice1
+            input(f"{NAME2} press enter to roll the dice.")
             dice2 = int(random.randint(1, 6))
             print(f"Dice rolled {dice2}")
-            score2 = score2+dice2
-            print(f"{player1}'s Score: {score1}\n{player2}'s Score: {score2}")
+            TOTAL_player2 = TOTAL_player2+dice2
+            print(f"{NAME1}'s Score: {TOTAL_player1}\n{NAME2}'s Score: {TOTAL_player2}")
 
-        if score1 > target:
-            score1 = score1-dice1
+        if  TOTAL_player1 > GOAL:
+            TOTAL_player1 = TOTAL_player1-dice1
 
-        if score2 > target:
-            score2 = score2-dice2
+        if TOTAL_player2 > GOAL:
+            TOTAL_player2 = TOTAL_player2-dice2
 
-        if score2 == target:
-            print(f"{player2} you won the game!!")
+        if TOTAL_player2 == GOAL:
+            print(f"##### {NAME2} you won the game #####")
             break
 
-        if score1 == target:
-            print(f"{player1} you won the game!!")
+        if TOTAL_player1 == GOAL:
+            print(f"###### {NAME1} you won the game #####")
             break
 
-    replay = input("Press Y to play again or press any other key to exit ==> ")
+    VICTORY = input("##### ENTER j TO PLAY AGIAN OR ENTER ANY KEY TO EXIT ######")
 
-    if replay.lower() == "y":
+    if VICTORY.lower()=="j":
         continue
     else:
+        print("*************")
         print("Thank you for playing!!")
         break
