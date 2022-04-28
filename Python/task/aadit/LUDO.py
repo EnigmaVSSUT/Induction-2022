672019bc8244b051bfe57ce746ff2e58b90478a2
Aadit kar <aaditkar007@gmail.com>
2022-04-28 11:53:57 +0530

    Python Task Completed

--- Python/task/aadit/LUDO.py
+++ Python/task/aadit/LUDO.py
@@ -1,43 +1,50 @@
 import random
 
 while True:
-    player1 = input("Player 1 enter your name ==> ")
-    player2 = input("Player 2 enter your name ==> ")
-    target = int(input("Enter the target number: "))
-    score1 = 0
-    score2 = 0
+    player1 = input("Player 1 name : ")
+    player2 = input("Player 2 name : ")
+    goal = int(input("Enter your goal : "))
+  
+    s1 = 0
+    s2 = 0
+    
     while True:
-        if (score1 < target) and (score2 < target):
-            input(f"{player1} press Enter to roll the dice.")
-            dice1 = int(random.randint(1, 6))
-            print(f"Dice rolled {dice1}")
-            score1 = score1+dice1
-            input(f"{player2} press enter to roll the dice.")
-            dice2 = int(random.randint(1, 6))
-            print(f"Dice rolled {dice2}")
-            score2 = score2+dice2
-            print(f"{player1}'s Score: {score1}\n{player2}'s Score: {score2}")
+        if (s1 < goal) and (s2 < goal):
+            input(f"{player1} press Enter to roll the dice -")
+            roll1 = int(random.randint(1, 6))
+           
+            print(f"It rolled {roll1}")
+            s1 = s1+roll1
+           
+            input(f"{player2} press enter to roll the dice -")
+            roll2 = int(random.randint(1, 6))
 
-        if score1 > target:
-            score1 = score1-dice1
+            print(f"It rolled {roll2}")
+            s2 = s2+roll2
+           
+            print(f"{player1}'s Score: {s1}\n{player2}'s Score: {s2}")
 
-        if score2 > target:
-            score2 = score2-dice2
+        if s1 > goal:
+            s1 = s1-roll1
 
-        if score2 == target:
-            print(f"{player2} you won the game!!")
+        if s2 > goal:
+            s2 = s2-roll2
+
+        if s1 == goal:
+            print(f"{player1} is the Winner")
             break
 
-        if score1 == target:
-            print(f"{player1} you won the game!!")
+        if s2 == goal:
+            print(f"{player1} is the Winner")
             break
 
-    replay = input("Press Y to play again or press any other key to exit ==> ")
+    X = input("Enter A to replay or any other key to Exit :")
 
-    if replay.lower() == "y":
+    if X.lower() == "a":
         continue
+    
     else:
-        print("Thank you for playing!!")
+        print("O(∩_∩)O  Thank you for playing  O(∩_∩)O")
         break
 
     
