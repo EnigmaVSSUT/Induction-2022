462c758947226026ec9466797d1a5748600a3031
Sugyani Manjari Dash <99403465+Sugyani31@users.noreply.github.com>
2022-04-28 11:51:18 -0700

    python task done

--- Python/task/ludo.py
+++ Python/task/ludo.py
@@ -1,41 +1,29 @@
 import random
+# t is the target for the game
+t = int(input("target is:-"))
+ #s1,s2 are the scores of player1 and player2
+s1=0
+s2=0
+while (s1<t) and (s2<t):
+    #here two players rolling their dice alternatively and their score will be added to their previous score
+    input("player_1 press enter to roll the dice")
+    dice1=int(random.randint(1,6))
+    s1=s1+dice1
+    print(dice1)
+    input("Player_2 press enter to roll the dice")
+    dice2=int(random.randint(1,6))
+    dice2=s2+dice2
+    print(dice2)
+    #if score will exceed the target then recently generated score will be discarded
+    #winner will be declared only if player score is equal to target
+    if s1> t :
+        s1=s1-dice1
+    if s1==t :
+        print("Player_1 is the winner")
+        break
 
-while True:
-    player1 = input("enter player_1 name :- ")
-    player2 = input("enter player_2 name :- ")
-    target = int(input("Enter the target :- "))
-    scr1 = 0
-    scr2 = 0
-    while True:
-        if (scr1 < target) and (scr2 < target):
-            input(f"{player1} press Enter to roll the dice.")
-            dice1 = int(random.randint(1, 6))
-            print(f"Dice rolled {dice1}")
-            scr1 = scr1+dice1
-            input(f"{player2} press enter to roll the dice.")
-            dice2 = int(random.randint(1, 6))
-            print(f"Dice rolled {dice2}")
-            scr2 = scr2+dice2
-            print(f"{player1}'s Score: {scr1}\n{player2}'s Score: {scr2}")
-
-        if scr1 > target:
-            scr1 = scr1-dice1
-
-        if scr2 > target:
-            scr2 = scr2-dice2
-
-        if scr2 == target:
-            print(f"{player2} you won the game!!")
-            break
-
-        if scr1 == target:
-            print(f"{player1} you won the game!!")
-            break
-
-    replay = input("Press Y to play again or press any other key to exit ==> ")
-
-    if replay.lower() == "y":
-        continue
-    else:
-        print("Thank you for playing..")
+    if s2>t :
+        s2=s2-dice2
+    if s2==t :
+        print("Player_2 is the winner")
         break
