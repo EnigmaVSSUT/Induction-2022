106caef35e8a1200379eab7cbc0f1b8f2c4780ae
Arpita-45 <99397114+Arpita-45@users.noreply.github.com>
2022-04-28 07:44:47 -0700

    Create

---
+++ Python/task/ArpitaTripathy/python.py
@@ -0,0 +1,40 @@
+import random
+while True:
+     player1 = input("enter player1 name: ")
+     player2 = input("enter player2 name: ")
+     target = int(input("Enter the target number: "))
+     score1 = 0
+     score2 = 0
+     while True:
+         if (score1 < target) and (score2 < target):
+             input(f"{player1} press Enter to roll the dice.")
+             dice1 = int(random.randint(1, 6))
+             print(f"Dice rolled {dice1}")
+             score1 = score1+dice1
+             input(f"{player2} press enter to roll the dice.")
+             dice2 = int(random.randint(1, 6))
+             print(f"Dice rolled {dice2}")
+             score2 = score2+dice2
+             print(f"{player1}'s Score: {score1} {player2}'s Score: {score2}")
+
+         if score1 > target:
+             score1 = score1-dice1
+
+         if score2 > target:
+             score2 = score2-dice2
+
+         if score2 == target:
+             print(f"{player2} you won the game")
+             break
+
+         if score1 == target:
+             print(f"{player1} you won the game")
+             break
+
+     replay = input("Press Y to play again or press any other key to exit  ")
+
+     if replay.lower() == "y":
+         continue
+     else:
+         print("Thank you for playing")
+         break
