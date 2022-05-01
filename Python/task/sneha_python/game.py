669e86f9e100ce03347bb3be1fb7ebc1a96d24ff
snehab03 <ammit.poitrix@gmail.com>
2022-05-01 20:56:26 +0530

    python_task

--- Python/task/sneha_python/game.py
+++ Python/task/sneha_python/game.py
@@ -2,6 +2,7 @@ import random
 g=int(input("enter goal score:"))
 player1=input("enter player_1 name:")
 player2=input("enter player_2 name:")
+print(".........GAME START.........")
 score1=0
 score2=0
 def dice():
@@ -9,25 +10,38 @@ def dice():
 
 while score1<=g and score2<=g:
     player1roll=dice()
-    score1+=player1roll 
-    print("score1",score1)
+    print(f"{player1} scored :{score1}")
+    score1+=player1roll
+     
+    #print(player1,"score1:",score1)
+    print("****************")
 
     if score1<=g:
        
         if score1==g:
-            print("score1",score1)
+            #print(player1,"score1:",score1)
+            print(f"{player1} scored :{score1}")
+            print("****************")
             print(" congratulations!!!",player1,"wins") 
+            print("..................................")
+            print("THANK YOU FOR PLAYING!!!")
             break
-    
+        
     player2roll=dice()
+    print(f"{player2} scored :{score2}")
     score2+=player2roll
-    print("score2",score2)
+    #print(player2,"score2",score2)
+    print("****************")
    
     if score2<=g:
       
      if score2==g:
-         print("score2",score2 )
+         print(f"{player2} scored :{score2}")
+         #print(player2,"score2:",score2 )
+         print("****************")
          print(" congratulations!!!",player2,"wins")
+         print("..................................")
+         print("THANK YOU FOR PLAYING!!!")
          break
      
     if score1>g:
